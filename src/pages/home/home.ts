9a30711b40aae9e34f47e10a335160837f1e776b
wesleifelix <wesleifelix@gmail.com>
2018-07-10 10:21:01 -0300

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,50 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { ArvoreProvider } from '../../providers/arvore/arvore'
+import { IArvore } from '../../interface/IArvore';
+import { IConfig } from '../../interface/IConfig';
+import { StorageProvider } from '../../providers/storage/storage';
 
-/**
- * Generated class for the HomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html',
+  templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	_arvores: IArvore[];
+	_confgis:IConfig;
+	_imgPaths = "http://mercado8.dlinkddns.com/verdejar/public/images/a/";
+  constructor(public navCtrl: NavController, public providerArvore: ArvoreProvider,public storageProvider: StorageProvider) {
+
+  	this.providerArvore.getAll().subscribe(retorno => {
+
+  		console.log(retorno);
+				//Salva os dados dentro do banco de dados
+				this._arvores = retorno;
+				this.storageProvider.SetStorage('VerdejarAvores', this._arvores);
+			},error=>{
+				console.log(error.error);
+				console.log(error.texte);
+			}
+	);
+
+	this.providerArvore.getConfig().subscribe(retorno => {
+		console.log(retorno);
+				//Salva os dados dentro do banco de dados
+				this._confgis = retorno;
+				this.storageProvider.SetStorage('VerdejarConfigs', this._confgis);
+			}
+			,error=>{
+				console.log(error.error);
+				console.log(error.texte);
+			}
+	);
+
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
-  }
+   ionViewDidLoad() {
+	
+	}
 
 }

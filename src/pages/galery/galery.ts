12faa4c36ad4b1dbde9a6d7c91b675056599fd7c
Thayller Vilela Cintra <thayllervilelacintra@yahoo.com.br>
2018-07-18 14:34:17 -0300

    Add files via upload

--- src/pages/galery/galery.ts
+++ src/pages/galery/galery.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the GaleryPage page.
@@ -15,11 +15,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class GaleryPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+   // Variável responsável por guardar a lista de imagens
+   images : any[] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public modal: ModalController) {
+    // Lista de imagens criada a partir de um placeholder, você deve substituir aqui pela sua lista que pode vir de um provider
+    for (var _i = 10; _i < 60; _i++) {
+      this.images.push({
+          id: _i,
+          image_path: 'https://picsum.photos/100/100?image=' + _i
+      })
+    }
   }
 
-  ionViewDidLoad() {
+  /*ionViewDidLoad() {
     console.log('ionViewDidLoad GaleryPage');
-  }
+  }*/
+  openModal(urlImage) {
+
+      const myModalData = {
+        descricao: 'descricao da arvore',
+        url: urlImage
+      };
+
+      const myModal: Modal = this.modal.create('ModalPage', { data: myModalData });
+
+      myModal.present();
+
+      /*myModal.onDidDismiss((data) => {
+        console.log("I have dismissed.");
+        console.log(data);
+      });
+
+      myModal.onWillDismiss((data) => {
+        console.log("I'm about to dismiss");
+        console.log(data);
+      });*/
+
+    }
 
 }
